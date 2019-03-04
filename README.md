7a418d44222b394de26906f42f744bd29c09fae5
Daren Wesolowski <daren.wesolowski@icloud.com>
2019-03-04 15:29:00 -0500

    Added multi message support with random option

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ Configure the default server chat / join / leave messages.
 
 #### Supported Versions: 1.13+
 
+- Multi line support
+- Randon option
+
 ## Installation
 
 To use this plugin place the .jar and any other files in your plugins directory and restart the server.
--- src/main/java/com/github/dwesolowski/eventmessages/PlayerJoin.java
+++ src/main/java/com/github/dwesolowski/eventmessages/PlayerJoin.java
@@ -6,8 +6,12 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import java.util.List;
+import java.util.Random;
+
 public class PlayerJoin implements Listener {
 
+    private int counter = 0;
     private final EventMessages plugin;
 
     PlayerJoin(EventMessages pl) {
@@ -18,10 +22,21 @@ public class PlayerJoin implements Listener {
     public void onPlayerJoin(final PlayerJoinEvent e) {
         final Player p = e.getPlayer();
         final String join = this.plugin.getConfig().getString("Join").replace("{player_name}", p.getName());
-        for (final String messages : this.plugin.getConfig().getStringList("Message")) {
-            final String msg = messages.replace("{player_name}", p.getName());
+        boolean random = this.plugin.getConfig().getBoolean("random-mode");
+
+        if (this.counter == this.plugin.getConfig().getConfigurationSection("Messages").getKeys(false).size()) {
+            this.counter = 0;
+        }
+        if (random) {
+            this.counter = new Random().nextInt(this.plugin.getConfig().getConfigurationSection("Messages").getKeys(false).size());
+        }
+
+        final List<String> Message = this.plugin.getConfig().getStringList("Messages." + this.counter + ".Message");
+        for (final String s : Message) {
+            final String msg = s.replace("{player_name}", p.getName());
             p.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
         }
+        ++this.counter;
         e.setJoinMessage(ChatColor.translateAlternateColorCodes('&', join));
     }
 }
